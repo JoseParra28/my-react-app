ec2e41128a9bb295bb6f3eae5579541bfd9cfd1f
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-17 20:58:29 +0000

    react
    filter

--- src/List.jsx
+++ src/List.jsx
@@ -1,11 +1,19 @@
 
 function List(){
-    const fruits = [{"apple"}, 
-                    {"orange"}, 
-                    {"coconut"},
-                     {"bananas"}, 
-                     {"pineapple"};
-    const listItem = fruits.map(fruits => <li>Fruit</li>);
+    const fruits = [{id: 1 , name:"apple", calories: 95}, 
+                    {id: 2 , name:"orange", calories: 45}, 
+                    {id: 3 , name:"coconut", calories: 120},
+                    {id: 4 , name:"bananas", calories:99}, 
+                    {id: 5 , name:"pineapple", calories: 65}];
+
+    // fruits.sort((a,b) =>a.name.localeCompare(b.name))
+    fruits.sort((a,b) =>a.calories - b.calories)
+    // fruits.sort((a,b) => a.id - b.id)
+
+    const lowCalFruits = fruits.filter(fruit => fruit.calories < 100)
+    const listItem = lowCalFruits.map(lowCalFruits => <li key={lowCalFruits.id}>
+                                                            {lowCalFruits.name}: &nbsp;
+                                                            <b>{lowCalFruits.calories}</b></li>);
 
     return(<ol>{listItem}</ol>);
 }
