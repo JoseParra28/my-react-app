778ea33c92c99096d834e372b7621b8e396693b4
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-17 14:58:24 +0000

    react
    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import Card from "./Card";
+import Button from "./Button";
 
 
 
 function App() {
   return (
     <>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
+   <Button/>
+  
     </>
   );
 }
