122391acc7354e851d2b205dd4a6cb18dd6528ae
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-17 11:33:16 +0000

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx';
-import Food from './Food.jsx';
+import Card from "./Card";
+
 
 
 function App() {
   return (
     <>
-    <Header/>
-    <Food/>
-    <Footer/>
-    
-
+   <Card/>
     </>
   );
 }
---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+
+function Card (){
+    return (
+        <div className="card">
+            <img src="https://picsum.photos/id/237/450/300?grayscale" alt="this is profile picture" />
+            <h2>JoseDev</h2>
+            <p>I code and workout</p>
+
+        </div>
+    );
+
+}
+
+export default Card
