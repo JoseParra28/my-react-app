fa29e8f1e13347937696e888eed0eb32c469ca02
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-17 15:44:55 +0000

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Button from "./Button";
+import Student from "./Student";
 
 
 
 function App() {
   return (
     <>
-   <Button/>
+   <Student name='Nicolas' age={30} isStudent={false}/>
+   <Student name='Patrick' age={38} isStudent={true}/>
+   <Student name='Jose' age={33} isStudent={true}/>
+   <Student name='Sara' age={50} isStudent={true}/>
     </>
   );
 }
+
 
 export default App
