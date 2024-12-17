4aac4d13d4cf8eb9ffc51efcfa43767baf7176f0
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-17 10:54:36 +0000

    react
    header and footer

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,15 @@ function Header(){
     return (
         <header>
             <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Services</li>
+                    <li><a href="#"></a>Contact</li>
+                    <li><a href="#"></a>Home</li>
+                </ul>
+            </nav>
         </header>
     )
 }
